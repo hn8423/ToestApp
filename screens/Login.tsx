20a4f64ce785b1dcf4b06ef0b4ff90581527f83c
hn8423 <rolenche@gmail.com>
2022-12-27 18:09:16 +0900

    user-agent 적용

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -30,6 +30,7 @@ import {
   TabActions,
   useFocusEffect,
 } from '@react-navigation/native'
+import {SafeAreaView} from 'react-native-safe-area-context'
 const chartHeight = Dimensions.get('window').height
 
 const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
@@ -146,6 +147,7 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
 
   const isEmailValid = useMemo(() => {
     const reg =
+      // eslint-disable-next-line no-control-regex
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     return reg.test(email)
   }, [email])
@@ -215,8 +217,8 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
     )
     setTimeout(() => {
       // Get cookies for a url
-      CookieManager.get(`${baseURL}/mobile/signin`).then(cookies => {
-        console.log('CookieManager.get =>', cookies)
+      CookieManager.get(`${baseURL}/mobile/signin`).then(cookiesPart => {
+        console.log('CookieManager.get =>', cookiesPart)
       })
     }, 500)
     const fn = () => {
@@ -322,8 +324,13 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
           <WebView
             ref={webRef}
             source={{
-              uri: `${baseURL}/mobile/signin`,
+              uri: `${baseURL}/mobile`,
             }}
+            userAgent={
+              Platform.OS === 'android'
+                ? 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36'
+                : 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+            }
             style={
               {
                 // display: 'none',
@@ -331,12 +338,14 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
             }
             onNavigationStateChange={navState => {
               console.log(navState.url)
+              if (navState.url === 'https://dev.toest.me/mobile#') {
+              }
             }}
           />
-          <Header />
+          {/* <Header />
           <ScrollView>
             <View {...style.container}>
-              {/* <View {...style.social}>
+              <View {...style.social}>
                 <TouchableHighlight>
                   <Image
                     {...style.socialLogo}
@@ -355,7 +364,7 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
                     source={require('../assets/images/login/kakao.png')}
                   />
                 </TouchableHighlight>
-              </View> */}
+              </View>
               <View {...style.input}>
                 <Text {...style.inputTitle}>Email</Text>
                 <TextInput
@@ -405,7 +414,7 @@ const LogIn: SC<LoginStackParams, 'LogIn'> = ({navigation}) => {
                 </View>
               </View>
             </View>
-          </ScrollView>
+          </ScrollView> */}
         </>
       )}
     </>
